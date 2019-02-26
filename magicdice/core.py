802509bd1bae3c7c376968e0c3b205b38978f19f
Emre Yilmaz <mail@emreyilmaz.me>
2019-02-26 23:39:43 +0300

    Add a flag to Magicdice.bid to ensure the tx picked up by the website

--- magicdice/core.py
+++ magicdice/core.py
@@ -1,3 +1,5 @@
+import time
+
 from steem.account import Account
 from steem.transactionbuilder import TransactionBuilder
 from steembase import operations
@@ -17,6 +19,20 @@ class Magicdice:
         self.account = account
         self.active_key = active_key
 
+    def _ensure_pickup(self, bid_block_id, bid_transaction_id):
+        """ Ensures a bid picked up by magic-dice.
+        """
+        bets = self.api_client.bets(self.account, {"limit": 50})
+        for bet in bets:
+            if bet["refTransactionId"] == bid_transaction_id:
+                return bet
+
+        raise ValueError(f"This bid is not picked up by magic dice! "
+                         f"{bid_block_id}#{bid_transaction_id}")
+
+    def poll_roll(self):
+        pass
+
     @property
     def steem_account(self):
         """Return a fresh steem.Account instance
@@ -26,7 +42,7 @@ class Magicdice:
         return Account(self.account, steemd_instance=self.steem)
 
     def bid(self, prediction_type, prediction_number,
-            amount, client_seed=None):
+            amount, client_seed=None, ensure_pickup=False):
         """Create a bid and broadcast to the blockchain.
 
         :param prediction_type (str): under or over
@@ -48,7 +64,14 @@ class Magicdice:
         tb.appendSigner(self.account, "active")
         tb.sign()
         response = self.steem.broadcast_transaction_synchronous(tb.json())
-        return response.get("id"), response.get("block_num")
 
-    def verify(self, transaction_id):
-        pass
+        # ensure the transaction is picked up by magic-dice.
+        # sleep block generation duration + 1 seconds for safety
+        if ensure_pickup:
+            time.sleep(4)
+            pickup_details = self._ensure_pickup(
+                response.get("block_num"), response.get("id"))
+            return response.get("id"), response.get("block_num"),\
+                   pickup_details
+
+        return response.get("id"), response.get("block_num")
